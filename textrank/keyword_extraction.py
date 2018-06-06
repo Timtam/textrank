b139728dddc858a56f548a5f3c7e559b4ae3d9e1
Toni Barth <toni.barth@gisa.de>
2018-06-06 12:10:47 +0200

    combining multiword keyphrases

--- textrank/keyword_extraction.py
+++ textrank/keyword_extraction.py
@@ -11,7 +11,7 @@ def filter_tags(tags):
   allowed = ["JJ", "NN", "NNP"]
   # we also filter tags which don't contain letters at all, or are simply too short
   r=re.compile(r"[a-zA-Z]")
-  return [i for i in tags if i[1] in allowed and r.search(i[0]) and len(i[0]) > 3]
+  return [i for i in tags if i[1] in allowed and r.search(i[0])]
 
 def uniquify(tags):
   # each tag will only be returned once
@@ -38,17 +38,41 @@ def build_graph(words):
   
   return graph
 
-def extract_keywords(text):
+def extract_keywords(text, max=20):
+  # extract max keywords for the given text
+
   word_tokens = nltk.word_tokenize(text)
   pos_tagged = nltk.pos_tag(word_tokens)
   pos_tagged = filter_tags(pos_tagged)
   unique_tags = uniquify(pos_tagged)
   graph = build_graph(unique_tags)
   page_rank = networkx.pagerank(graph, weight="weight")
-  keywords = sorted(page_rank, key=page_rank.get, reverse=True)
+  keywords = [k[0] for k in sorted(page_rank, key=page_rank.get, reverse=True)]
 
   # amount of keywords will be one third of all unique keywords
   amount = len(unique_tags) / 3
-  keywords = keywords[0:amount+1]
+  if amount > max:
+    amount = max
   
-  return [k[0] for k in keywords]
+  keywords = keywords[0:amount+1]
+
+  final_keyphrases = []
+  
+  processed = []
+  
+  ui = unique_tags.pop(False)[0]
+
+  while unique_tags:
+    uj = unique_tags.pop(False)[0]
+    if ui in keywords and uj in keywords:
+      final_keyphrases.append(ui + " " + uj)
+      processed.append(ui)
+      processed.append(uj)
+    else:
+      if ui in keywords and not ui in processed:
+        final_keyphrases.append(ui)
+      if not unique_tags and uj in keywords and uj not in processed:
+        final_keyphrases.append(uj)
+    ui = uj
+
+  return final_keyphrases
